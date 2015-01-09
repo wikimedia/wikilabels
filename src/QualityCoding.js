008e1e119b6a5a66f1294d2c939d0834352153f1
Helder <he7d3r@gmail.com>
2015-01-09 14:32:56 -0200

    Make the code more general
    Also rename a few variables and add some comments to the code.

--- src/QualityCoding.js
+++ src/QualityCoding.js
@@ -53,7 +53,7 @@
 			'qc-submit': 'Submeter',
 			'qc-not-implemented': 'Este recurso ainda não está implementado.'
 		}
-	};
+	}, fields;
 
 	function notImplemented( e ) {
 		var $target = $( e.target );
@@ -67,58 +67,114 @@
 		alert( 'The value is ' + $target.data( 'qc-value' ) );
 	}
 
-	// FIXME: Remove this by something real
+	// FIXME: Replace this by an actual call to some API which returns revision ids
 	function getRandomSet( size, done ) {
-		var i, rev, list = [];
+		var i, j, rev, list = [];
 		size = size || 100;
 		done = done || 70;
 		for ( i = 0; i < size; i++ ) {
-			rev = { id: Math.floor( Math.random() * 1000000 ) };
+			rev = {
+				id: Math.floor( Math.random() * 1000000 ),
+				// One key for each of the things we want to predict (vandalism, good-faith, quality, etc)
+				fields: {}
+			};
 			if ( i <= done ) {
 				// TODO:
 				// * Should an 'undefined' value mean 'unknown'?
-				//   In that case, this can continue being a boolean
+				//   In that case, this could be a boolean
 				// * Or one should explicitly set a value (e.g. 0) to mean 'unknown'?
-				//   Then use -1, 0 and 1 as possible values.
-				rev.damaging = Math.floor( Math.random() * 2 ) === 1;
-				rev.goodFaith = Math.floor( Math.random() * 2 ) === 1;
-				// Assume good faith (if the edit is good)
-				rev.goodFaith = rev.goodFaith || !rev.damaging;
+				//   In case there are just three possibilities, we could use -1, 0 and 1 instead of 0, 1 and 2?
+				// FIXME: these names should not be hardcoded
+				// TODO: Should we hardcode common sense such as "not damaging implies good faith"?
+				for ( j = 0; j < fields.length; j++ ) {
+					// Classes from 0 to (N-1), where N is the number of options for this field
+					rev.fields[ fields[j].id ] = Math.floor( Math.random() * fields[j].options.length );
+				}
 			}
 			list.push( rev );
 		}
 		return list;
 	}
-	function addRandomExamples() {
-		var i, isDamaging, isGoodFaith, $icon,
+
+	function showWorkSet() {
+		var i, j, field, idx, $icon, className,
 			$bar = $( '.qc-progress' ),
-			randomSet = getRandomSet();
-		for ( i = 0; i < randomSet.length; i++ ) {
+			// FIXME: this will probably be assyncronous (obtained from some API)
+			workSet = getRandomSet();
+		for ( i = 0; i < workSet.length; i++ ) {
 			$icon = $( '<div>' );
-			isDamaging = randomSet[i].damaging;
-			isGoodFaith = randomSet[i].goodFaith;
-			if ( isDamaging === undefined ) {
-				$icon.addClass( 'qc-damaging-unsure' );
-			} else {
-				$icon.toggleClass( 'qc-damaging', isDamaging );
-			}
-			if ( isGoodFaith === undefined ) {
-				$icon.addClass( 'qc-good-faith-unsure' );
-			} else {
-				$icon.toggleClass( 'qc-good-faith', isGoodFaith );
+			for ( j = 0; j < fields.length; j++ ) {
+				field = fields[j].id;
+				idx = workSet[i].fields[ field ];
+				if( idx !== undefined ){
+					className = 'qc-' + field + '-' + fields[j].options[ idx ].value;
+					$icon.addClass( className );
+				}
 			}
 			$icon.attr(
 				'title',
 				mw.msg(
 					'qc-icon-title',
-					randomSet[i].id,
-					( isDamaging ? mw.msg( 'qc-damaging-yes' ) : mw.msg( 'qc-damaging-no' ) ),
-					( isGoodFaith ? mw.msg( 'qc-good-faith-yes' ) : mw.msg( 'qc-good-faith-no' ) )
+					workSet[i].id,
+					// FIXME: Each revision is scored according to more than one aspect
+					// so we need to pass a variable number of parameters to the messages
+					mw.msg( className )
 				)
 			);
 			$bar.append( $icon );
 		}
-		$( '.qc-progress div' ).css( 'width', ( 100 / randomSet.length ) + '%' );
+		$( '.qc-progress div' ).css( 'width', ( 100 / workSet.length ) + '%' );
+	}
+
+	function loadConfig(){
+		return [
+			{
+				id: 'damaging',
+				class: 'revcoding.ui.RadioButtons',
+				label: mw.msg( 'qc-damaging' ),
+				help: mw.msg( 'qc-damaging-title' ),
+				options: [
+					{
+						label: mw.msg( 'qc-damaging-yes' ),
+						tooltip: mw.msg( 'qc-damaging-yes-title' ),
+						value: 'yes'
+					},
+					{
+						label: mw.msg( 'qc-damaging-unsure' ),
+						tooltip: mw.msg( 'qc-damaging-unsure-title' ),
+						value: 'unsure'
+					},
+					{
+						label: mw.msg( 'qc-damaging-no' ),
+						tooltip: mw.msg( 'qc-damaging-no-title' ),
+						value: 'no'
+					}
+				]
+			},
+			{
+				id: 'good-faith',
+				class: 'revcoding.ui.RadioButtons',
+				label: mw.msg( 'qc-good-faith' ),
+				help: mw.msg( 'qc-good-faith-title' ),
+				options: [
+					{
+						label: mw.msg( 'qc-good-faith-yes' ),
+						tooltip: mw.msg( 'qc-good-faith-yes-title' ),
+						value: 'yes'
+					},
+					{
+						label: mw.msg( 'qc-good-faith-unsure' ),
+						tooltip: mw.msg( 'qc-good-faith-unsure-title' ),
+						value: 'unsure'
+					},
+					{
+						label: mw.msg( 'qc-good-faith-no' ),
+						tooltip: mw.msg( 'qc-good-faith-no-title' ),
+						value: 'no'
+					}
+				]
+			}
+		];
 	}
 
 	function load() {
@@ -126,53 +182,9 @@
 				.addClass( 'qc-ui' ),
 			$submit = $( '<input class="mw-ui-button mw-ui-constructive" type="submit">' )
 				.val( mw.msg( 'qc-submit' ) ),
-			fields = [
-				{
-					id: 'damaging',
-					label: mw.msg( 'qc-damaging' ),
-					help: mw.msg( 'qc-damaging-title' ),
-					options: [
-						{
-							label: mw.msg( 'qc-damaging-yes' ),
-							tooltip: mw.msg( 'qc-damaging-yes-title' ),
-							value: 'yes'
-						},
-						{
-							label: mw.msg( 'qc-damaging-unsure' ),
-							tooltip: mw.msg( 'qc-damaging-unsure-title' ),
-							value: 'unsure'
-						},
-						{
-							label: mw.msg( 'qc-damaging-no' ),
-							tooltip: mw.msg( 'qc-damaging-no-title' ),
-							value: 'no'
-						}
-					]
-				},
-				{
-					id: 'good-faith',
-					label: mw.msg( 'qc-good-faith' ),
-					help: mw.msg( 'qc-good-faith-title' ),
-					options: [
-						{
-							label: mw.msg( 'qc-good-faith-yes' ),
-							tooltip: mw.msg( 'qc-good-faith-yes-title' ),
-							value: 'yes'
-						},
-						{
-							label: mw.msg( 'qc-good-faith-unsure' ),
-							tooltip: mw.msg( 'qc-good-faith-unsure-title' ),
-							value: 'unsure'
-						},
-						{
-							label: mw.msg( 'qc-good-faith-no' ),
-							tooltip: mw.msg( 'qc-good-faith-no-title' ),
-							value: 'no'
-						}
-					]
-				}
-			],
 			field, i, j, id, $feature, $group;
+		// When moving this around, make sure that mw.messages.set is called before mw.msg
+		fields = loadConfig();
 		$ui.append(
 			$( '<div>' )
 				.text( mw.msg( 'qc-work-set' ) ),
@@ -205,7 +217,7 @@
 		}
 		$ui.append( $submit );
 		$( 'table.diff' ).first().before( $ui );
-		addRandomExamples();
+		showWorkSet();
 	}
 
 	if ( mw.util.getParamValue( 'diff' ) !== null ) {

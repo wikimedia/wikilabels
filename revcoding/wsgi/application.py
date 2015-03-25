072b443c234fd485855abc341d80e57bba3bb932
halfak <aaron.halfaker@gmail.com>
2015-03-25 17:17:42 -0500

    Adds demo JSON output to WSGI server

--- revcoding/wsgi/application.py
+++ revcoding/wsgi/application.py
@@ -1,6 +1,8 @@
 import json
-from flask import Flask, request
+
 import mwoauth
+from flask import Flask, jsonify, request
+
 
 def configure(config=None):
     config = config or {}
@@ -40,7 +42,12 @@ def configure(config=None):
         if 'create' in request.args:
             create_campaign(wiki, request.args['create'])
         else:
-            return "Get wiki not implemented yet."
+            return jsonify(
+                {
+                    "wiki": wiki,
+                    "campaigns": [345, 376]
+                }
+            )
 
     @app.route("/<wiki>/", methods=["POST"])
     def create_campaign(wiki, campaign):
@@ -57,21 +64,78 @@ def configure(config=None):
         if 'assign' in request.args:
             return assign_workset(wiki, campaign)
         else:
-            return "Get campaign not implemented yet."
+            return jsonify(
+                {
+                    "wiki": wiki,
+                    "campaign": {
+                        "id": campaign,
+                        "name": "Edit quality -- 2014 10k sample",
+                        "form": "damaging_and_goodfaith",
+                        "view": "diff_to_previous",
+                        "created": 1234567830,
+                        "labels_per_task": 1,
+                        "tasks": {
+                            "labels": 10000,
+                            "completed": 5723,
+                            "assigned": 300
+                        }
+                    }
+                }
+            )
 
     @app.route("/<wiki>/<int:campaign>/", methods=["POST"])
     def assign_workset(wiki, campaign):
         """
         Assigns and gathers metadata for a particular workset
         """
-        return "Assign workset not implemented yet."
+        return get_workset(wiki, campaign, 9001)
 
     @app.route("/<wiki>/<int:campaign>/<int:workset>/", methods=["GET"])
     def get_workset(wiki, campaign, workset):
         """
         Gathers metadata for a particular workset
         """
-        return "Get workset not implemented yet."
+        if 'abandon' in request.args:
+            return jsonify({"success": True})
+
+        if 'expand' in request.args:
+            campaign = {
+                "id": campaign,
+                "name": "Edit quality -- 2014 10k sample",
+                "form": "damaging_and_goodfaith",
+                "view": "diff_to_previous",
+                "created": 1234567830,
+                "labels_per_task": 1,
+                "tasks": {
+                    "labels": 10000,
+                    "completed": 5723,
+                    "assigned": 300
+                }
+            }
+
+        else:
+            campaign = {"id": campaign}
+
+        return jsonify(
+            {
+                "wiki": wiki,
+                "campaign": campaign,
+                "workset": {
+                    "id": 1027,
+                    "user_id": 607828,
+                    "assigned": "2015-02-21T13:45:56Z",
+                    "expiration": "2015-02-22T13:45:56Z",
+                    "tasks": [
+                        {"id": 102, "data": {"rev_id": 3456780}},
+                        {"id": 103, "data": {"rev_id": 3456781}},
+                        {"id": 104, "data": {"rev_id": 3456782}},
+                        {"id": 105, "data": {"rev_id": 3456783}},
+                        {"id": 106, "data": {"rev_id": 3456784}},
+                        {"id": 107, "data": {"rev_id": 3456785}}
+                    ]
+                }
+            }
+        )
 
     @app.route("/<wiki>/<int:campaign>/<int:workset>/<int:task>/", methods=["GET"])
     def get_task(wiki, campaign, workset, task):
@@ -80,8 +144,46 @@ def configure(config=None):
         """
         if 'label' in request.args:
             return label_task(wiki, campaign, workset, task, request.args['label'])
+
         else:
-            return "Get task not implemented yet."
+            if 'expand' in request.args:
+                campaign = {
+                    "id": campaign,
+                    "name": "Edit quality -- 2014 10k sample",
+                    "form": "damaging_and_goodfaith",
+                    "view": "diff_to_previous",
+                    "created": 1234567830,
+                    "labels_per_task": 1,
+                    "tasks": {
+                        "labels": 10000,
+                        "completed": 5723,
+                        "assigned": 300
+                    }
+                }
+
+            else:
+                campaign = {"id": campaign}
+
+            return jsonify(
+                {
+                    "wiki": wiki,
+                    "campaign": campaign,
+                    "task": {
+                        "id": task,
+                        "data": {"rev_id": 3456780}
+                    },
+                    "labels": [
+                        {
+                            "user_id": 607828,
+                            "timestamp": 1244597890,
+                            "data": {
+                                "damaging": False,
+                                "good-faith": True
+                            }
+                        }
+                    ]
+                }
+            )
 
     @app.route("/<wiki>/<int:campaign>/<int:workset>/<int:task>/",
                methods=["PUT", "POST"])
@@ -91,6 +193,41 @@ def configure(config=None):
         """
         label = json.loads(request.form.get('label', label))
 
-        return "Label task not implemented yet ({0})".format(label)
+        if 'expand' in request.args:
+            campaign = {
+                "id": campaign,
+                "name": "Edit quality -- 2014 10k sample",
+                "form": "damaging_and_goodfaith",
+                "view": "diff_to_previous",
+                "created": 1234567830,
+                "labels_per_task": 1,
+                "tasks": {
+                    "labels": 10000,
+                    "completed": 5723,
+                    "assigned": 300
+                }
+            }
+            task = {
+                "id": task,
+                "data": {"rev_id": 3456780}
+            }
+
+        else:
+            campaign = {"id": campaign}
+            task = {"id": task}
+
+        return jsonify({
+            "wiki": wiki,
+            "campaign": campaign,
+            "task": task,
+            "label": {
+                "user_id": 607828,
+                "timestamp": 1244597890,
+                "data": {
+                    "damaging": False,
+                    "good-faith": True
+                }
+            }
+        })
 
     return app

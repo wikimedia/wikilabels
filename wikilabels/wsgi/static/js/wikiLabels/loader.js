daf5be0d8eded31fa4cb965a2e9d0c588912fbb6
Helder <he7d3r@gmail.com>
2015-05-02 18:09:17 -0300

    Wait for document to be ready before interacting with the page

--- wikilabels/wsgi/static/js/wikiLabels/loader.js
+++ wikilabels/wsgi/static/js/wikiLabels/loader.js
@@ -1,20 +1,22 @@
-( function (mw, $) {
-	// Check if we should try to load Wiki Labels
-	if ($('#wikilabels-home').length) {
-		mw.loader.using(
-		['oojs', 'oojs-ui', 'json', 'mediawiki.action.history.diff'],
-			function () {
-				mw.loader.load( '//localhost:8080/labels/gadget/WikiLabels.css', 'text/css' );
-				$.getScript(
-					"//localhost:8080/labels/gadget/WikiLabels.js",
-					function () {
-						wikiLabels.config.update( {
-							serverRoot: "//localhost:8080/labels"
-						} );
-						wikiLabels.home = new wikiLabels.Home($('#wikilabels-home'));
-					}
-				);
-			}
-		);
-	}
+(function(mw, $){
+  $(function(){
+    // Check if we should try to load Wiki Labels
+    if($('#wikilabels-home').length){
+      mw.loader.using(
+        ['oojs', 'oojs-ui', 'json', 'mediawiki.action.history.diff'],
+        function(){
+          mw.loader.load( '//localhost:8080/labels/gadget/WikiLabels.css', 'text/css' );
+          $.getScript(
+            "//localhost:8080/labels/gadget/WikiLabels.js",
+            function(){
+              wikiLabels.config.update( {
+                serverRoot: "//localhost:8080/labels"
+              } );
+              wikiLabels.home = new wikiLabels.Home($('#wikilabels-home'));
+            }
+          );
+        }
+      );
+    }
+  });
 }(mediaWiki, jQuery));

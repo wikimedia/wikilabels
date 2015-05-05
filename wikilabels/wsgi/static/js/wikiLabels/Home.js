e7e06ae5e4dd77008001c18d60a11230238b85fd
halfak <aaron.halfaker@gmail.com>
2015-05-05 18:04:57 -0500

    Fixes menu seeking issue in Home

--- wikilabels/wsgi/static/js/wikiLabels/Home.js
+++ wikilabels/wsgi/static/js/wikiLabels/Home.js
@@ -13,7 +13,7 @@
 		}
 
 		this.$element = $element;
-		this.$menu = this.$element.find(".menu");
+		this.$menu = this.$element.find("> .menu");
 		if ( this.$menu === undefined || this.$menu.length !== 1 ) {
 			throw "#" + WL.config.prefix + "home > .menu must be a single defined element";
 		}

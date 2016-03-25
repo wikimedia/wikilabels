ac1ed012db62471ae8e316d0de725433fc7043bb
halfak <aaron.halfaker@gmail.com>
2016-03-25 13:55:53 -0500

    Removes https by default from dev_server and adds it optionally.

--- wikilabels/utilities/dev_server.py
+++ wikilabels/utilities/dev_server.py
@@ -5,17 +5,17 @@ Starts a web server for hosting access to a set of scorers.
 
 Usage:
     server (-h | --help)
-    server [--port=<num>] [--verbose] [--config=<path>]
+    server [--port=<num>] [--config=<path>] [--ssl] [--verbose] 
 
 Options:
     -h --help        Print this documentation
     --port=<num>     The port number to start the server on [default: 8080]
     --config=<path>  The path to a yaml config file
                      [default: config/wikilabels-localdev.yaml]
+    --ssl            If set, run the server on ad-hoc SSL (https)
     --verbose        Print logging information
 """
 import docopt
-
 import yamlconf
 
 from ..wsgi import server
@@ -29,9 +29,14 @@ def main(argv=None):
     else:
         config = None
 
+    if args['--ssl']:
+        kwargs = {'ssl_context': "adhoc"}
+    else:
+        kwargs = {}
+
     app = server.configure(config)
     app.debug = True
     app.run(host="0.0.0.0",
             port=int(args['--port']),
             debug=True,
-            ssl_context="adhoc")
+            **kwargs)

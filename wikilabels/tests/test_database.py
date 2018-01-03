c24bddff6f8023b241586f7d9870f5d7af812d49
eisenhaus335 <greenzone332@outlook.com>
2018-01-03 10:24:29 +0700

    Create test_database.py

---
+++ wikilabels/tests/test_database.py
@@ -0,0 +1,13 @@
+import pytest
+import os
+from wikilabels.database import db
+import yamlconf
+
+item1 = {'wiki' : "cawiki", 'name' : "ching", 'form' : "chan", 'view' : "bivicyt", 'labels_per_task' : 1,'task_per_task' : 50, 'active' : True, 'info_url': "https://www.mediawiki.org/wiki/ORES#Edit_quality"}
+db_ = db.DB(1, 5, database = "wikilabels", user = "wikilabels", password = "wikilabels-admin") #args untuk minconn dan maxconn. kwargs untuk beri nama user, dns, dll ada di laporan travis no 25
+user = 608705
+campaign = db.campaigns
+def test_campaign_showitem():
+    item = db_.campaigns.create(item1.get('wiki'), item1.get('name'), item1.get('form'), item1.get('view'), item1.get('labels_per_task'), item1.get('task_per_task'), item1.get('active'), item1.get('info_url'))
+    print(item.get('id'))
+    assert False

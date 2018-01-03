2d3890f82c0e2fc1e63b6459de7aa2d923bdedce
Ricky Setiawan <greenzone332@outlook.com>
2018-01-03 11:30:43 +0700

    test_create

--- wikilabels/tests/test_database.py
+++ wikilabels/tests/test_database.py
@@ -7,6 +7,7 @@ item1 = {'wiki' : "cawiki", 'name' : "ching", 'form' : "chan", 'view' : "bivicyt
 db_ = db.DB(1, 5, database = "wikilabels", user = "wikilabels", password = "wikilabels-admin") #args untuk minconn dan maxconn. kwargs untuk beri nama user, dns, dll ada di laporan travis no 25
 user = 608705
 campaign = db.campaigns
+
 def test_campaign_showitem():
     item = db_.campaigns.create(item1.get('wiki'), item1.get('name'), item1.get('form'), item1.get('view'), item1.get('labels_per_task'), item1.get('task_per_task'), item1.get('active'), item1.get('info_url'))
     print(item.get('id'))

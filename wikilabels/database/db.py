2764c8225188ce0a8174d94238a27d9dff2a5c71
Ricky Setiawan <greenzone332@outlook.com>
2018-01-06 00:57:05 +0700

    test_create

--- wikilabels/database/db.py
+++ wikilabels/database/db.py
@@ -22,6 +22,11 @@ class DB:
         self.tasks = Tasks(self)
         self.labels = Labels(self)
         self.logger = logging.getLogger(__name__)
+        with self.transaction() as transactor:
+            cursor = transactor.cursor()
+            cursor.execute("""SELECT setval('campaign_id_seq'
+                        ,(SELECT max(id) FROM campaign));
+                        """)
 
     def _initialize_pool(self):
         if self.pool is None:
--- wikilabels/tests/test_database.py
+++ wikilabels/tests/test_database.py
@@ -4,14 +4,90 @@ from wikilabels.database import db
 import yamlconf
 
 item1 = {'wiki' : "cawiki", 'name' : "ching", 'form' : "chan", 'view' : "bivicyt", 'labels_per_task' : 1,'task_per_task' : 50, 'active' : True, 'info_url': "https://www.mediawiki.org/wiki/ORES#Edit_quality"}
-db_ = db.DB(1, 5, database = "wikilabels", user = "wikilabels", password = "wikilabels-admin") #args untuk minconn dan maxconn. kwargs untuk beri nama user, dns, dll ada di laporan travis no 25
+dbs = db.DB(1, 5, database = "wikilabels", user = "wikilabels", password = "wikilabels-admin") #args untuk minconn dan maxconn. kwargs untuk beri nama user, dns, dll ada di laporan travis no 25
 user = 608705
 
 
-def test_campaign_showitem():
-    item = db_.campaigns.create(item1.get('wiki'), item1.get('name'), item1.get('form'), item1.get('view'), item1.get('labels_per_task'), item1.get('task_per_task'), item1.get('active'), item1.get('info_url'))
-    print(item.get('id'))
-    gets = db.Campaigns.get(item.get('id'), True)
-    print(gets)
-    print(item1.get(6))
-    assert False
+def test_campaign_create():
+    assert dbs.campaigns.create(item1.get('wiki'), item1.get('name'),
+                                item1.get('form'), item1.get('view'),
+                                item1.get('labels_per_task'), item1.get('task_per_task'),
+                                item1.get('active'), item1.get('info_url'))
+
+def test_campaign_checkwikiexists():
+    assert dbs.campaigns.wiki_name_exists(item1.get('wiki'), item1.get('name'))
+
+def test_campaign_getitems():
+    if not dbs.campaigns.get(item1.get(1)):
+        print('Item id 1 for unknown reason is cannot be found')
+        assert False
+    assert True
+
+def test_campaign_statsfor():
+    item = dbs.campaigns.stats_for(1)
+    if item is None:
+        print('Unkown Campaign or there are no tasks')
+        assert False
+    assert True
+
+def test_campaign_hasopentasks():
+    assert dbs.campaigns.has_open_tasks(1, user)
+
+def test_campaign_forwiki():
+    assert dbs.campaigns.for_wiki('enwiki')
+
+def test_campaign_foruser():
+    assert dbs.campaigns.for_user(608705)
+
+def test_campaign_wikis():
+    assert dbs.campaigns.wikis()
+
+def test_campaign_users():
+    assert dbs.campaigns.users()
+
+def test_labels_upsertupdate():
+    item = dbs.labels.upsert(1, 608705, '{"damaging": true, "good-faith": true}')
+    if item['data'] is not '{"damaging": true, "good-faith": false}':
+        assert True
+
+def test_labels_CRUD():
+    if dbs.labels.insert(3, user, '{"damaging": true, "good-faith": true}') is not None:
+        if dbs.labels.update(3, user, '{"damaging": false, "good-faith": true}') is not None:
+            if dbs.labels.clear_data(3, user, '{"damaging": false, "good-faith": true}') is not None:
+                assert True
+
+def tests_worksets_get():
+    if dbs.worksets.get(1) is not None:
+        assert True
+
+def tests_worksets_statsfor():
+    if dbs.worksets.stats_for(1) is not None:
+        assert True
+
+def tests_worksets_forcampaign():
+    if dbs.worksets.for_campaign(1) is not None:
+        assert True
+
+def tests_worksets_foruser():
+    if dbs.workset.for_user() is not None:
+        assert True
+
+def test_worksets_openworksetsforuser()
+    if dbs.worksets.open_workset_for_user(1, user) is not None:
+        assert True
+
+def test_worksets_assign():
+    if dbs.worksets.assign(2, user) is not None:
+        assert True
+
+def test_worksets_users():
+    if dbs.worksets.users() is not None:
+        assert True
+
+def test_worksets_abandon():
+    if dbs.worksets.abandon(2, user) is not None:
+        assert True
+
+def test_worksets_abandontask():
+    if dbs.worksets.abandon_task(1, user, 1) is not None:
+        assert True

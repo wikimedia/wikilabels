e9f8f06da86c0f42319053a652d9ed2a20b4bf91
eisenhaus335 <greenzone332@outlook.com>
2018-01-18 19:40:07 +0700

    Update test_database.py

--- wikilabels/tests/test_database.py
+++ wikilabels/tests/test_database.py
@@ -13,8 +13,8 @@ user = 608705
 
 def test_campaign_create():
     cursor.execute("""
-            SELECT setval('campaign_id_seq',(SELECT max(id) FROM campaign));
-                        """)
+    SELECT setval('campaign_id_seq',(SELECT max(id) FROM campaign));
+    """)
     assert dbs.campaigns.create(item1.get('wiki'), item1.get('name'),
                                 item1.get('form'), item1.get('view'),
                                 item1.get('labels_per_task'),
@@ -88,8 +88,8 @@ def test_worksets_openworksetsforuser():
 
 def test_worksets_assign():
     cursor.execute("""
-        SELECT setval('workset_id_seq',(SELECT max(id) FROM workset));
-        """)
+    SELECT setval('workset_id_seq',(SELECT max(id) FROM workset));
+    """)
     assert dbs.worksets.assign(2, user)
 
 

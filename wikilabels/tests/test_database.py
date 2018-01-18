df038abfa246bce1eb4e9e651c5a53c8ef2eab03
eisenhaus335 <greenzone332@outlook.com>
2018-01-18 21:08:19 +0700

    Update test_database.py

--- wikilabels/tests/test_database.py
+++ wikilabels/tests/test_database.py
@@ -18,7 +18,6 @@ def test_campaign_create():
         SELECT setval('campaign_id_seq',(SELECT max(id) FROM campaign));
         """)
         assert dbs.campaigns.create(item1.get('wiki'), item1.get('name'),
-        assert dbs.campaigns.create(item1.get('wiki'), item1.get('name'),
                                     item1.get('form'), item1.get('view'),
                                     item1.get('labels_per_task'),
                                     item1.get('task_per_task'),
@@ -28,7 +27,7 @@ def test_campaign_create():
 def test_campaign_checkwikiexists():
     assert dbs.campaigns.wiki_name_exists(item1.get('wiki'), item1.get('name'))
 
-                                    
+
 def test_campaign_getitems():
     assert dbs.campaigns.get(1)
 

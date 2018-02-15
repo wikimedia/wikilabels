eba983dc803cc5d02923a431fbe0c518089c59ff
Aaron Halfaker <aaron.halfaker@gmail.com>
2018-02-15 17:17:17 -0600

    Fixes wikisource dbname --> url pattern. (#227)

--- wikilabels/util/tests/test_wikimedia.py
+++ wikilabels/util/tests/test_wikimedia.py
@@ -6,3 +6,4 @@ def test_host_from_dbname():
     assert wikimedia.host_from_dbname('fawiki') == 'fa.wikipedia.org'
     assert wikimedia.host_from_dbname('wikidatawiki') == 'wikidata.org'
     assert wikimedia.host_from_dbname('labswiki') == 'wikitech.wikimedia.org'
+    assert wikimedia.host_from_dbname('bnwikisource') == 'bn.wikisource.org'

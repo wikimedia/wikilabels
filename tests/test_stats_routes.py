de8494b48862c115171672f0eb6c353cd147c271
Andy Craze <acraze@wikimedia.org>
2019-06-20 08:58:23 -0700

    flake8 fix

--- tests/test_stats_routes.py
+++ tests/test_stats_routes.py
@@ -7,5 +7,6 @@ def test_stats_wiki_campaign(client):
     # test archived campaign
     assert client.get("/stats/enwiki/7")._status_code == 200
 
+
 def test_stats(client):
     assert client.get("/stats/")._status_code == 200
